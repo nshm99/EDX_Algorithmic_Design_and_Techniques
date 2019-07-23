37ec7d4d2fe56b0abb0eed861fe25bb98bc9c280
nargeshm99 <nargeshm99@outlook.com>
2019-07-23 21:07:24 +0430

    Do week three assignments
    third week assignments

---
+++ week_3/A4/A4Tests/ProgramTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A4.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        //1.............................................................................
+
+        [TestMethod(),Timeout(1000)]
+        [DeploymentItem(@"TestData","A4_TestData")]
+        public void ChangingMony1Test()
+        {
+            TestTools.RunLocalTest("A4", Program.ProcessChangingMony1, "TD1");
+        }
+
+        //2.............................................................................
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A4_TestData")]
+        public void MaximizingLoot2Test()
+        {
+            TestTools.RunLocalTest("A4", Program.ProcessMaximizingLoot2, "TD2");
+        }
+        
+        //3.............................................................................
+        [TestMethod(),Timeout(1000)]
+        [DeploymentItem(@"TestData", "A4_TestData")]
+
+        public void MaximizingOnlineAdRevenue3Test()
+        {
+            TestTools.RunLocalTest("A4", Program.ProcessMaximizingOnlineAdRevenue3, "TD3");
+        }
+
+        //4..............................................................................
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A4_TestData")]
+
+        public void CollectingSignatures4Test()
+        {
+            TestTools.RunLocalTest("A4", Program.ProcessCollectingSignatures4, "TD4");
+        }
+
+        //5..............................................................................
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A4_TestData")]
+
+        public void MaximizeNumberOfPrizePlaces5Test()
+        {
+            TestTools.RunLocalTest("A4", Program.ProcessMaximizeNumberOfPrizePlaces5, "TD5");
+        }
+
+        //6...........................................................................
+
+        [TestMethod(), Timeout(1000)]
+        [DeploymentItem(@"TestData", "A4_TestData")]
+
+        public void MaximizeSalary6Test()
+        {
+            TestTools.RunLocalTest("A4", Program.ProcessMaximizeSalary6, "TD6");
+        }
+    }
+}
